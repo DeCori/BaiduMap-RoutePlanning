6c62f30e38aa7023960fadea0075dbbb38c7fbf4
MuChen <tao_wht@163.com>
2017-03-08 17:27:24 +0800

    empty project
    empty project

---
+++ BaiduMap_RP/BaiduMap_RP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiduMap_RP
+//
+//  Created by Wang Hongtao on 2017/3/8.
+//  Copyright © 2017年 Muchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
